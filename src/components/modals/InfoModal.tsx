053fa3d168399d7534c8cbebc2c719b7a68cd4ba
adamlaz <adamlaz@gmail.com>
2022-01-19 08:18:09 -0500

    🏷️ make words insurance related

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -62,42 +62,42 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
                   </Dialog.Title>
                   <div className="mt-2">
                     <p className="text-sm text-gray-500">
-                      Guess the WORDLE in 6 tries. After each guess, the color
+                      Guess the INSUR-DLE in 6 tries. After each guess, the color
                       of the tiles will change to show how close your guess was
                       to the word.
                     </p>
 
                     <div className="flex justify-center mb-1 mt-4">
-                      <Cell value="W" status="correct" />
+                      <Cell value="G" status="correct" />
                       <Cell value="E" />
-                      <Cell value="A" />
-                      <Cell value="R" />
-                      <Cell value="Y" />
+                      <Cell value="I" />
+                      <Cell value="C" />
+                      <Cell value="O" />
                     </div>
                     <p className="text-sm text-gray-500">
-                      The letter W is in the word and in the correct spot.
+                      The letter G is in the word and in the correct spot.
                     </p>
 
                     <div className="flex justify-center mb-1 mt-4">
-                      <Cell value="P" />
-                      <Cell value="I" />
+                      <Cell value="W" />
+                      <Cell value="H" />
+                      <Cell value="E" />
+                      <Cell value="E" />
                       <Cell value="L" status="present" />
-                      <Cell value="O" />
-                      <Cell value="T" />
                     </div>
                     <p className="text-sm text-gray-500">
                       The letter L is in the word but in the wrong spot.
                     </p>
 
                     <div className="flex justify-center mb-1 mt-4">
-                      <Cell value="V" />
-                      <Cell value="A" />
-                      <Cell value="G" />
-                      <Cell value="U" status="absent" />
+                      <Cell value="Q" />
+                      <Cell value="U" />
+                      <Cell value="O" />
+                      <Cell value="T" status="absent" />
                       <Cell value="E" />
                     </div>
                     <p className="text-sm text-gray-500">
-                      The letter U is not in the word in any spot.
+                      The letter T is not in the word in any spot.
                     </p>
                   </div>
                 </div>

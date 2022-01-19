a787f7b107c7ec6eb568eb35116d237a7ba6e012
adamlaz <adamlaz@gmail.com>
2022-01-19 08:19:50 -0500

    💬 replace wordle with insurdle

--- src/components/modals/AboutModal.tsx
+++ src/components/modals/AboutModal.tsx
@@ -61,9 +61,9 @@ export const AboutModal = ({ isOpen, handleClose }: Props) => {
                   </Dialog.Title>
                   <div className="mt-2">
                     <p className="text-sm text-gray-500">
-                      This is an open source clone of the game Wordle -{' '}
+                      This is an open source clone of the game Wordle that uses ~Insurance words. This was forked from @hannahcode by @adamlaz -{' '}
                       <a
-                        href="https://github.com/hannahcode/wordle"
+                        href="https://github.com/adamlaz/wordle/"
                         className="underline font-bold"
                       >
                         check out the code here
